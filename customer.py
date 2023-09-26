a7e145e2b30f334210a002a7027b9f91b75366bd
lastsamurai24 <lastsamurai24@icloud.com>
2023-09-26 11:16:28 +0900

    #6

--- customer.py
+++ customer.py
@@ -122,3 +122,85 @@ ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 print(ken.info_csv())
 print(tom.info_csv())
 print(ieyasu.info_csv())
+
+print("------------------課題C-5----------------------")
+
+
+# 5
+class Customer:
+    def __init__(self, first_name, family_name, age=None):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.age = age
+        self.name = first_name + " " + family_name
+
+    def full_name(self):
+        return f"{self.first_name} {self.family_name}"
+
+    def entry_fee(self):
+        if self.age is None:
+            return None
+        if self.age < 3:
+            return 0
+        elif self.age < 20:
+            return 1000
+        elif 20 <= self.age < 65:
+            return 1500
+        elif 65 <= self.age:
+            return 1200
+
+    def info_csv(self):
+        return f"{self.full_name()},{self.age},{self.entry_fee()}"
+
+
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+minion = Customer(first_name="Minion", family_name="Min", age=1)
+print(ken.info_csv())
+print(tom.info_csv())
+print(ieyasu.info_csv())
+print(minion.info_csv())
+
+print("------------------課題C-6----------------------")
+
+
+# 6
+class Customer:
+    def __init__(self, first_name, family_name, age=None):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.age = age
+        self.name = first_name + " " + family_name
+
+    def full_name(self):
+        return f"{self.first_name} {self.family_name}"
+
+    def entry_fee(self):
+        if self.age is None:
+            return None
+        if self.age < 3:
+            return 0
+        elif self.age < 20:
+            return 1000
+        elif 20 <= self.age < 65:
+            return 1500
+        elif 65 <= self.age < 75:
+            return 1200
+        elif 75 <= self.age:
+            return 500
+
+    def info_csv(self):
+        return f"{self.full_name()},{self.age},{self.entry_fee()}"
+
+
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+minion = Customer(first_name="Minion", family_name="Min", age=1)
+hayao = Customer(first_name="Hayao", family_name="Miyazaki", age=82)
+print(ken.info_csv())
+print(tom.info_csv())
+print(ieyasu.info_csv())
+print(minion.info_csv())
+print(hayao.info_csv())
